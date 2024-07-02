a2b1434a5fe8014f6a7c49d4c8b3a55529bcc428
unknown <nattawatnattawatmoo@gmail.com>
2024-07-02 10:58:22 +0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,28 @@
+export default function Home() {
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row text-center">
+          <div className="p-3 col-12 bg-primary">
+            Navbar
+          </div>
+        </div>
+      <div className="row text-center">
+        <div className="col-12 bg-info p-5">
+         Home
+        </div>
+      </div>
+      <div className="row text-center">
+        <div className="col-md-4 bg-success p-5 ">column</div>
+        <div className="col-md-4 bg-warning p-5 ">column</div>
+        <div className="col-md-4 bg-danger p-5 ">column</div>
+        </div>
+      </div>
+    <div className="row text-center">
+      <div className="p-5 col-12 bg-primary ">
+        Nattawat
+        </div>
+      </div>
+    </>
+  );
+}
